f14135b1111e61a830cd735cf2126b7e8de683f8
Rosemary <rdt13@ic.ac.uk>
2017-11-15 18:15:13 +0000

    All graphs now replotted for correct orientation of cost function

--- hw2_template_dev.py
+++ hw2_template_dev.py
@@ -71,18 +71,20 @@ def newton_test(xg,display=False,i=1):
         Maxy=max(Y)+1
         [Xj,Yj]=np.linspace(Minx,Maxx,200),np.linspace(Miny,Maxy,200)
         #calculate noiseless cost function at each point on 2D grid
-        j=[[cost.costj([xi,yi]) for yi in Yj] for xi in Xj]
+        j=[[cost.costj([xi,yi]) for xi in Xj] for yi in Yj]
 
         f, (p1,p2) = plt.subplots(1,2)
-        p1.contourf(Yj, Xj, j, locator=ticker.LogLocator(), cmap=cm.GnBu)
+        p1.contourf(Xj, Yj, j, locator=ticker.LogLocator(), cmap=cm.GnBu)
         p1.plot(X,Y,'g',marker='d')
         p1.set_xlim(min(X)-1,max(X)+1)
         p1.set_xlabel('X1-location')
         p1.set_ylabel('X2-location')
+        p1.set_title('Convergence Path')
         p2.plot(np.linspace(0,len(X)-1,len(X)),np.sqrt((X-xf[0])**2+(Y-xf[1])**2))
         p2.set_xlabel('Iteration number')
         p2.set_ylabel('distance from converged minimum')
-        plt.suptitle('Rosemary Teague, Newton_test, initial guess ='+str(xg)+' \n Rate of convergence of a cost function')
+        p2.set_title('Rate')
+        plt.suptitle('Rosemary Teague, Newton_test, initial guess ='+str(xg)+' \n Convergence of a cost function')
         plt.tight_layout(pad=4)
         plt.savefig('hw22'+str(i), dpi=700)
 
@@ -92,7 +94,7 @@ def newton_test(xg,display=False,i=1):
     return xf,jf,output
 
 
-def bracket_descent_test(xg,display=False,i=1):
+def bracket_descent_test(xg,display=False,compare=False,i=1):
     """ Use bracket-descent to minimize cost function defined in cost module
     Input variable xg is initial guess for location of minimum. When display
     is true, 1-2 figures comparing the B-D and Newton steps should be generated
@@ -102,29 +104,57 @@ def bracket_descent_test(xg,display=False,i=1):
     cost.c_noise=False
     hw2.tol=10**(-6)
     hw2.itermax=1000
+    t1=0;t2=0;t3=0;t4=0
+    t1 = time()
     hw2.bracket_descent(xg)
+    t2 = time()
     X,Y=hw2.xpath
     xf=[X[-1],Y[-1]]
     jf=hw2.jpath[-1]
+    d1=np.sqrt((X-xf[0])**2+(Y-xf[1])**2)
 
 
     if display:
+        Minx=min(X)-1
+        Maxx=max(X)+1
+        Miny=min(Y)-1
+        Maxy=max(Y)+1
+        [Xj,Yj]=np.linspace(Minx,Maxx,200),np.linspace(Miny,Maxy,200)
+        #calculate noiseless cost function at each point on 2D grid
+        j=[[cost.costj([xi,yi]) for xi in Xj] for yi in Yj]
         f, (p1,p2) = plt.subplots(1,2)
-        p1.plot(X,Y)
+        p1.contourf(Xj, Yj, j, locator=ticker.LogLocator(), cmap=cm.GnBu)
+        p1.plot(X,Y,'g',marker='d')
         p1.set_xlabel('X1-location')
         p1.set_ylabel('X2-location')
-        p2.plot(np.linspace(0,len(X)-1,len(X)),np.sqrt((X-xf[0])**2+(Y-xf[1])**2))
+        p1.set_title('Convergence Path')
+        p2.plot(np.linspace(0,len(X)-1,len(X)),d1)
         p2.set_xlabel('Iteration number')
         p2.set_ylabel('distance from converged minimum')
+        p2.set_title('Rate')
         plt.suptitle('Rosemary Teague, bracket_descent_test, initial guess ='+str(xg)+' \n Rate of convergence of a cost function')
         plt.tight_layout(pad=4)
-        plt.savefig('hw23'+str(i), dpi=700)
+        plt.savefig('hw231_'+str(i), dpi=700)
 
-
-
+    if compare:
+        plt.close('all')
+        t3= time()
+        xf2,jf2,i2=hw2.newton(xg)
+        t4 = time()
+        X2,Y2=hw2.xpath
+        d2=np.sqrt((X2-xf2[0])**2+(Y2-xf2[1])**2)
+        One,=plt.loglog(np.linspace(0,len(X)-1,len(X)),d1)
+        Two,=plt.loglog(np.linspace(0,len(X2)-1,len(X2)),d2)
+        One.set_label('Bracket Descent')
+        Two.set_label('Newton')
+        plt.xlabel('Iteration number')
+        plt.ylabel('Distance from converged minimum')
+        plt.legend()
+        plt.title('Rosemary Teague, bracket_descent_test, initial guess ='+str(xg)+' \n Comparison of Newton and Bracket Descent Methods')
+        plt.savefig('hw232_'+str(i), dpi=700)
 
     jf=cost.costj(xf)
-    return xf,jf
+    return xf,jf,t2-t1,t4-t3
 
 
 def performance():
@@ -179,13 +209,13 @@ def performance():
         Maxy=1+(max([max(xfbdy[(count-1)*4:count*4]),max(lbfgsy[(count-1)*4:count*4])])-1)*1.1
         [X,Y]=np.linspace(Minx,Maxx,200),np.linspace(Miny,Maxy,200)
         #calculate noiseless cost function at each point on 2D grid
-        j=[[cost.costj([xi,yi]) for yi in Y] for xi in X]
+        j=[[cost.costj([xi,yi]) for xi in X] for yi in Y]
         #create contour plots of cost functions with and without noise
         fig, p4 = plt.subplots()
         cp = p4.contourf(X, Y, j, locator=ticker.LogLocator(), cmap=cm.GnBu)
         cbar = fig.colorbar(cp)
-        BD,=p4.plot(xfbdx[(count-1)*4:count*4],xfbdy[(count-1)*4:count*4],'b',linestyle='None',marker='d',markersize=12)
-        Scipy,=p4.plot(lbfgsx[(count-1)*4:count*4],lbfgsy[(count-1)*4:count*4],'r',linestyle='None',marker='d',markersize=12)
+        BD,=p4.plot(xfbdx[(count-1)*4:count*4],xfbdy[(count-1)*4:count*4],'b',linestyle='None',marker='d',markersize=6)
+        Scipy,=p4.plot(lbfgsx[(count-1)*4:count*4],lbfgsy[(count-1)*4:count*4],'r',linestyle='None',marker='d',markersize=6)
         BD.set_label('Fortran Bracket Descent')
         Scipy.set_label('Scipy optimize L-BFGS-B')
         plt.legend(loc='upper left', fontsize='small')
@@ -248,8 +278,8 @@ if __name__ == '__main__':
     newton_test([5.,5.],display=True,i=2)
     newton_test([2.,2.],display=True,i=3)
 
-    bracket_descent_test([10.,10.],display=True,i=1)
-    bracket_descent_test([5.,5.],display=True,i=2)
-    bracket_descent_test([2.,2.],display=True,i=3)
+    bracket_descent_test([10.,10.],display=True,compare=True,i=1)
+    bracket_descent_test([5.,5.],display=True,compare=True,i=2)
+    bracket_descent_test([2.,2.],display=True,compare=True,i=3)
 
     performance()
